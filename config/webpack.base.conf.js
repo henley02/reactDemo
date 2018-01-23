b854f1cbc046378bdba33e88b7f8715a25058c9c
henley02 <410451447@qq.com>
2018-01-23 19:58:05 +0800

    add

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,9 @@
+let config = {
+    entry: {
+        'index': './src/index.js'
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx", ".css", ".pcss"],
+    }
+};
+module.exports = config;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from '../app/component/index/Index.js';
+
+ReactDOM.render(<h1>Hello World1111</h1>, document.getElementById('app'));
