1485356173a5d1922bcb8501a578d9dc808a3124
henley02 <410451447@qq.com>
2018-01-24 21:01:10 +0800

    [+]delete index.js

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import {Row, Col, Menu, Icon} from 'antd';
+import {Row, Col, Menu, Icon, Modal, Button, Tabs, message, Form, Input, Checkbox,} from 'antd';
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-export default class PCHeader extends React.Component {
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
+
+class PCHeader extends React.Component {
     constructor() {
         super();
         this.state = {
             current: 'top',
             mode: 'horizontal',
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userId: '',
         }
     }
 
+    setModalVisible(val) {
+        this.setState({modalVisible: val});
+    }
+
+    handleClick(e) {
+        console.log(e);
+        this.setState({current: e.key});
+        if (e.key == "register") {
+            this.setState({modalVisible: true});
+        }
+    }
+
+    handleSubmit() {
+
+    }
+
     render() {
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogined
+            ? <Menu.Item key="logout" className="register">
+                <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+                &nbsp;&nbsp;
+                <Link target="_blank">
+                    <Button type="dashed" htmlType="button">个人中心</Button>
+                </Link>
+                &nbsp;&nbsp;
+                <Button type="ghost" htmlType="button">退出</Button>
+            </Menu.Item>
+            :
+            <Menu.Item key="register" className="register">
+                <Icon type="appstore"/>注册/登录
+            </Menu.Item>;
+
         return (
             <header>
                 <Row>
@@ -24,7 +64,8 @@ export default class PCHeader extends React.Component {
                         </a>
                     </Col>
                     <Col span={16}>
-                        <Menu mode={this.state.mode} selectedKeys={[this.state.current]}>
+                        <Menu mode={this.state.mode} selectedKeys={[this.state.current]}
+                              onClick={this.handleClick.bind(this)}>
                             <Menu.Item key="top"> <Icon type="appstore"/>头条</Menu.Item>
                             <Menu.Item key="shehui"> <Icon type="appstore"/>社会</Menu.Item>
                             <Menu.Item key="guonei"> <Icon type="appstore"/>国内</Menu.Item>
@@ -33,7 +74,32 @@ export default class PCHeader extends React.Component {
                             <Menu.Item key="tiyu"> <Icon type="appstore"/>体育</Menu.Item>
                             <Menu.Item key="keji"> <Icon type="appstore"/>科技</Menu.Item>
                             <Menu.Item key="shishang"> <Icon type="appstore"/>时尚</Menu.Item>
+                            {userShow}
                         </Menu>
+
+                        <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible}
+                               onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)}
+                               okText="关闭">
+                            <Tabs type="card">
+                                <TabPane tab="注册" key="2">
+                                    <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                                        <FormItem label="账户">
+                                            <Input placeholder="请输入您的账户" {...getFieldProps('r_userName')}/>
+                                        </FormItem>
+                                        <FormItem label="密码">
+                                            <Input type="password"
+                                                   placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
+                                        </FormItem>
+                                        <FormItem label="确认密码">
+                                            <Input type="password"
+                                                   placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
+                                        </FormItem>
+                                        <Button type="primary" htmlType="submit">注册</Button>
+                                    </Form>
+                                </TabPane>
+                            </Tabs>
+
+                        </Modal>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
@@ -41,3 +107,5 @@ export default class PCHeader extends React.Component {
         )
     }
 }
+
+export default PCHeader = Form.create({})(PCHeader);
