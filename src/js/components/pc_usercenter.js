3fbd6a56f2430ea01002eb1daa65374defb034b3
henley02 <410451447@qq.com>
2018-02-13 17:57:23 +0800

    [+]edit upload file

--- src/js/components/pc_usercenter.js
+++ src/js/components/pc_usercenter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, Modal, Menu, Icon, Tabs, Upload} from 'antd';
+import {Row, Col, Modal, Menu, Icon, Tabs, Upload, Button} from 'antd';
 
 const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
@@ -8,22 +8,41 @@ export default class PCUserCenter extends React.Component {
     constructor() {
         super();
         this.state = {
+            fileList: [{
+                uid: -1,
+                name: 'xxx.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            }],
             previewVisible: false,
             previewImage: '',
+            uploading: false,
         }
     }
 
     handleCancel() {
-
+        this.setState({previewVisible: false})
     }
 
     render() {
+        const {uploading} = this.state;
+
         const props = {
             action: 'http://newsapi.gugujiankong.com/Handler.ashx',
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
             listType: 'picture-card',
+            onRemove: (file) => {
+                this.setState(({fileList}) => {
+                    const index = fileList.indexOf(file);
+                    const newFileList = fileList.slice();
+                    newFileList.splice(index, 1);
+                    return {
+                        fileList: newFileList,
+                    };
+                });
+            },
             onPreview: (file) => {
                 this.setState({previewImage: file.url, previewVisible: true});
             },
@@ -33,6 +52,7 @@ export default class PCUserCenter extends React.Component {
                 }));
                 return false;
             },
+            fileList: this.state.fileList,
         };
         return (
             <div>
@@ -53,9 +73,19 @@ export default class PCUserCenter extends React.Component {
                                         <div className="ant-upload-text">上传照片</div>
                                     </Upload>
                                     <Modal visible={this.state.previewVisible} footer={null}
-                                           onCancel={this.handleCancel}>
+                                           onCancel={this.handleCancel.bind(this)}>
                                         <img alt="example" style={{width: '100%'}} src={this.state.previewImage}/>
                                     </Modal>
+
+                                    <Button
+                                        className="upload-demo-start"
+                                        type="primary"
+                                        onClick={this.handleUpload}
+                                        disabled={this.state.fileList.length === 0}
+                                        loading={uploading}
+                                    >
+                                        {uploading ? 'Uploading' : 'Start Upload'}
+                                    </Button>
                                 </div>
                             </TabPane>
                         </Tabs>
